cf84426e10e2eeb7c1fe2954c71a35fc63ffb60e
chupetkiniv <chupetkiniv@tomato-pizza.ru>
2022-03-13 14:10:06 +0300

    update

--- main.py
+++ main.py
@@ -1,16 +1,48 @@
-# This is a sample Python script.
+import re
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from bs4 import BeautifulSoup
+
+with open("blank/index.html", encoding="utf-8") as file:
+    src = file.read()
+
+soup = BeautifulSoup(src, "lxml")
+# name = soup.find_all("div", class_="user__name")
+# for item in name:
+#     print(item.text.strip())
+
+# print(name)
+#
+# find_all_user_info = soup.find(class_="user__info").find_all("span")
+#
+# for item in find_all_user_info:
+#     print(item.text)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# social = soup.find(class_="social__networks").find("ul").find_all("a")
+# all_a = soup.find_all("a")
+# for item in all_a:
+#     item_text = item.text
+#     item_url = item.get("href")
+#     print(f'{item_text}:{item_url}')
+# post_div = soup.find(class_="post__text").find_parent()
+# print(post_div)
+
+# post_div = soup.find(class_="post__text").find_parent("div", 'user__post')
+# print(post_div)
+
+links = soup.find(class_="ewe__links").find_all("a")
+for link in links:
+    link_href = link.get("href")
+    link_href1 = link["href"]
+    link_data_attr = link.get("data-attr")
+    link_data_attr1 = link["data-attr"]
+    print(link_href+"/"+link_data_attr)
+    print(link_href1 + "/" + link_data_attr1)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+find_odejda = soup.find_all(text=re.compile("[Mm]agazine"))
+for find_item in find_odejda:
+    print(find_item.text)
+print(find_odejda)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
